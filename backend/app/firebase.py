bcc95241cd55ff41c0c9e4fa746bdff003e719e8
Waqar Akhtar <syed.waqar.akhtar08@gmail.com>
2026-07-13 21:29:40 +0530

    teemp

--- backend/app/firebase.py
+++ backend/app/firebase.py
@@ -1,12 +1,21 @@
 import os
 import json
 import firebase_admin
-
 from firebase_admin import credentials, auth
+
+print("=" * 60)
+print("FIREBASE_SERVICE_ACCOUNT exists:",
+      os.getenv("FIREBASE_SERVICE_ACCOUNT") is not None)
+
+print("Length:",
+      len(os.getenv("FIREBASE_SERVICE_ACCOUNT", "")))
+print("=" * 60)
 
 if not firebase_admin._apps:
 
     if os.getenv("FIREBASE_SERVICE_ACCOUNT"):
+
+        print("Using Render environment variable")
 
         firebase_config = json.loads(
             os.getenv("FIREBASE_SERVICE_ACCOUNT")
@@ -15,6 +24,8 @@ if not firebase_admin._apps:
         cred = credentials.Certificate(firebase_config)
 
     else:
+
+        print("Using local JSON file")
 
         cred = credentials.Certificate(
             "serviceAccountKey.json"
