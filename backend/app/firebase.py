c09e3954a292144b6972eb1e91240c0b86754dba
Waqar Akhtar <syed.waqar.akhtar08@gmail.com>
2026-07-13 21:10:21 +0530

    gooo

--- backend/app/firebase.py
+++ backend/app/firebase.py
@@ -1,9 +1,25 @@
+import os
+import json
 import firebase_admin
+
 from firebase_admin import credentials, auth
 
-# Initialize only once
 if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
+
+    if os.getenv("FIREBASE_SERVICE_ACCOUNT"):
+
+        firebase_config = json.loads(
+            os.getenv("FIREBASE_SERVICE_ACCOUNT")
+        )
+
+        cred = credentials.Certificate(firebase_config)
+
+    else:
+
+        cred = credentials.Certificate(
+            "serviceAccountKey.json"
+        )
+
     firebase_admin.initialize_app(cred)
 
 firebase_auth = auth
